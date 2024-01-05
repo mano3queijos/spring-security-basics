32b88ec11a47d5748ff28f05571793a82bc5ab92
emanuel almeida <emanuel.almeida@ucsal.edu.br>
2024-01-04 23:55:58 -0300

    ajeitando o pom

--- security/src/main/java/com/emanuel/seapp/HiController.java
+++ security/src/main/java/com/emanuel/seapp/HiController.java
@@ -1,12 +1,20 @@
 package com.emanuel.seapp;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HiController {
 
-public String sayHi(){
+    @GetMapping("/hi")
+    public String sayHi() {
+        return "hi";
+    }
 
-    return "hi";
-
-}
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "hello";
+    }
 
 
 }
